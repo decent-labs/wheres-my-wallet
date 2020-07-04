ea8749b410481c7b3f4a3cd3aa2aa21381b30267
Adam Gall <adam@decentlabs.io>
2020-07-03 22:42:10 -0400

    Beef up the test file, even though it's still terrible

--- test.js
+++ test.js
@@ -1,42 +1,75 @@
-const testSeed = "uno dos tres"
+const fs = require("fs")
+
+const testSeed = "uno dos tres cinco sies"
 const testWords = ["one", "two", "three", "four", "five"]
 
-const checkValidity = async seed => {
+const checkValidity = async (seed, updateIteration) => {
   const mnemonic = seed.join(" ")
-  const data = { mnemonic }
-  console.log(data)
+  updateIteration()
+  console.log(mnemonic)
 }
 
-const iterateWords = async (seedArray, wordlist, wordIndex, callback) => {
-  for (let i = 0; i < wordlist.length; i++) {
+const iterateWords = async (seedArray, wordlist, wordIndex, progress, progressIndex, callback) => {
+  let currentProgress = progress.indexes[progressIndex]
+
+  for (let i = currentProgress.wordListIndex; i < wordlist.length; i++) {
+    currentProgress.wordListIndex = i
     seedArray[wordIndex] = wordlist[i]
-    await callback(seedArray)
+    await callback(seedArray, () => progressFile(progress))
   }
+
+  currentProgress.wordListIndex = 0
 }
 
-const getBadWord = async (seedArray, otherIndex, callback) => {
-  for (let i = 0; i < seedArray.length; i++) {
-    if (otherIndex >= i) continue;
+const getWrongWord = async (seedArray, otherIndex, progress, progressIndex, callback) => {
+  let currentProgress = progress.indexes[progressIndex]
+
+  if (!currentProgress) {
+    progress.indexes[progressIndex] = { seedIndex: 0, wordListIndex: 0 }
+    currentProgress = progress.indexes[progressIndex]
+  }
+
+  for (let i = currentProgress.seedIndex; i < seedArray.length; i++) {
+    if (otherIndex >= i) continue
+
+    currentProgress.seedIndex = i
+    progressFile(progress)
+
     const badWord = seedArray[i]
     await callback(i, seedArray)
     seedArray[i] = badWord
   }
+
+  currentProgress.seedIndex = 0
+}
+
+const progressFile = progress => {
+  const progressPath = "./PROGRESS_TEST.json"
+  const write = (path, progress) => fs.writeFileSync(path, JSON.stringify(progress, null, "\t"))
+  if (progress) return write(progressPath, progress)
+  if (fs.existsSync(progressPath)) return require(progressPath)
+  const defaultData = { indexes: [] }
+  write(progressPath, defaultData)
+  return require(progressPath)
 }
 
 const findMoney = async (badSeed, wordlist) => {
+  const progress = progressFile()
   const seedArray = badSeed.split(" ")
 
-  await getBadWord(seedArray, -1, async (wordOneIndex, seedArray) => {
-    await iterateWords(seedArray, wordlist, wordOneIndex, async seedArray => {
-      await checkValidity(seedArray)
+  // single word replacement
+  await getWrongWord(seedArray, -1, progress, 0, async (wordOneIndex, seedArray) => {
+    await iterateWords(seedArray, wordlist, wordOneIndex, progress, 0, async (seedArray, updateIteration) => {
+      await checkValidity(seedArray, updateIteration)
     })
   })
 
-  await getBadWord(seedArray, -1, async (wordOneIndex, seedArray) => {
-    await getBadWord(seedArray, wordOneIndex, async (wordTwoIndex, seedArray) => {
-      await iterateWords(seedArray, wordlist, wordOneIndex, async seedArray => {
-        await iterateWords(seedArray, wordlist, wordTwoIndex, async seedArray => {
-          await checkValidity(seedArray)
+  // double word replacement
+  await getWrongWord(seedArray, -1, progress, 1, async (wordOneIndex, seedArray) => {
+    await getWrongWord(seedArray, wordOneIndex, progress, 2, async (wordTwoIndex, seedArray) => {
+      await iterateWords(seedArray, wordlist, wordOneIndex, progress, 1, async seedArray => {
+        await iterateWords(seedArray, wordlist, wordTwoIndex, progress, 2, async (seedArray, updateIteration) => {
+          await checkValidity(seedArray, updateIteration)
         })
       })
     })
